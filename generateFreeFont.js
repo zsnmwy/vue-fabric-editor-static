2839f556aedd6d93839874640e8652140e055ee2
zsnmwy <szlszl35622@gmail.com>
2023-10-11 05:48:10 +0800

    feat: change addr Signed-off-by: zsnmwy <szlszl35622@gmail.com>

--- generateFreeFont.js
+++ generateFreeFont.js
@@ -1,68 +1,74 @@
+const https = require("https");
+const { writeFile } = require("fs").promises;
 
-const https = require('https');
-const { writeFile } = require('fs').promises
-
-const freeFontUrl = 'https://wordshub.github.io/free-font/js/data.js'
-https.get(freeFontUrl, (response) => {
-  let str = '';
-  response.on('data', (chunk) => {
-    str += chunk;
+const freeFontUrl = "https://free-font.zsnmwy.net/js/data.js";
+https
+  .get(freeFontUrl, (response) => {
+    let str = "";
+    response.on("data", (chunk) => {
+      str += chunk;
+    });
+    response.on("end", () => {
+      const data = strToData(str);
+      // 筛选可直接使用的字体
+      const list = Object.keys(data).filter((key) => {
+        return data[key].download.includes(".ttf");
+      });
+      // 生成字体和json文件
+      generateCSS(list, data);
+      generateLocalCSS(list, data);
+      generateJSON(list, data);
+    });
+  })
+  .on("error", (error) => {
+    console.log("Error: " + error.message);
   });
-  response.on('end', () => {
-
-    const data = strToData(str)
-    // 筛选可直接使用的字体
-    const list = Object.keys(data).filter(key => {
-        return data[key].download.includes('.ttf')
-    })
-    // 生成字体和json文件
-    generateCSS(list, data)
-    generateLocalCSS(list, data)
-    generateJSON(list, data)
-  });
-
-}).on("error", (error) => {
-  console.log("Error: " + error.message);
-});
 // 字符串转对象
-function strToData(str){
-    const basic = str.split('var fontsMap =')[1]
-    const jsonStr = basic.substring(0, basic.length - 1);
-    var data = eval("(" + jsonStr + ")");
-    return data
+function strToData(str) {
+  const basic = str.split("var fontsMap =")[1];
+  const jsonStr = basic.substring(0, basic.length - 1);
+  var data = eval("(" + jsonStr + ")");
+  return data;
 }
 // 生成css文件
-function generateCSS(list,data){
-    let freeFont = ''
-    list.forEach(key => {
-      const newDownload = data[key].download.replace('https://github.com/wordshub/free-font/raw/master/', 'https://wordshub.github.io/free-font/')
-        freeFont += `@font-face {
+function generateCSS(list, data) {
+  let freeFont = "";
+  list.forEach((key) => {
+    const newDownload = data[key].download.replace(
+      "https://github.com/wordshub/free-font/raw/master/",
+      "https://free-font.zsnmwy.net/"
+    );
+    freeFont += `@font-face {
             font-family: '${data[key].name}';
             src: url('${newDownload}');
           }
-          `
-    })
-    writeFile('./font/free-font.css', freeFont, 'utf8')
+          `;
+  });
+  writeFile("./font/free-font.css", freeFont, "utf8");
 }
 
-function generateLocalCSS(list,data){
-  let freeFont = ''
-  list.forEach(key => {
-      const src = data[key].download.replace('https://github.com/', 'http://localhost:3000/fontFile/')
-      freeFont += `@font-face {
+function generateLocalCSS(list, data) {
+  let freeFont = "";
+  list.forEach((key) => {
+    const src = data[key].download.replace(
+      "https://github.com/",
+      "http://localhost:3000/fontFile/"
+    );
+    freeFont += `@font-face {
           font-family: '${data[key].name}';
           src: url('${src}');
         }
-        `
-  })
-  writeFile('./font/free-font-local.css', freeFont, 'utf8')
+        `;
+  });
+  writeFile("./font/free-font-local.css", freeFont, "utf8");
 }
 // 生成JSON文件
-function generateJSON(list,data){
-    let newData = {}
-    list.forEach(key => {
-        newData[key] = data[key]
-        newData[key].preview = 'https://wordshub.github.io/free-font/' + newData[key].preview
-    })
-    writeFile('./font/free-font.json', JSON.stringify(newData, null, 2), 'utf8')
+function generateJSON(list, data) {
+  let newData = {};
+  list.forEach((key) => {
+    newData[key] = data[key];
+    newData[key].preview =
+      "https://free-font.zsnmwy.net/" + newData[key].preview;
+  });
+  writeFile("./font/free-font.json", JSON.stringify(newData, null, 2), "utf8");
 }
